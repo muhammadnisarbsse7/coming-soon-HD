fbcc402cb5b76203c9097abe5ee06ecf9c9acd77
Muhammad Nisar <124270450+muhammadnisarbsse7@users.noreply.github.com>
2024-10-12 01:14:32 +0500

    Social media icons integrated

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,51 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
+import { DiscordIcon } from "./SVG/DiscordIcon";
+import { MediumIcon } from "./SVG/MediumIcon";
+import { TelegramIcon } from "./SVG/TelegramIcon";
+import { TwitterIcon } from "./SVG/TwitterIcon";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <>
-      <h1 className="text-3xl font-bold underline text-white bg-black rounded-3xl">
-        Hello world! sssss
-      </h1>
-    </>
+    <main>
+      <nav className="flex justify-between items-center mt-4 px-3 border-4">
+        <div className="font-poppins font-semibold text-[32px] text-[#494949]">
+          Hawala{" "}
+          <span className="font-poppins font-semibold text-[32px] text-[#FF3389] leading-[48px]">
+            DEX
+          </span>
+        </div>
+        <div className="flex gap-5">
+          <a
+            href="https://t.me/yourTelegramLink"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <TelegramIcon />
+          </a>
+          <a
+            href="https://twitter.com/yourTwitterLink"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <TwitterIcon />
+          </a>
+          <a
+            href="https://discord.gg/yourDiscordLink"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <DiscordIcon />
+          </a>
+          <a
+            href="https://medium.com/@yourMediumProfile"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            <MediumIcon />
+          </a>
+        </div>
+      </nav>
+    </main>
   );
 }
 
